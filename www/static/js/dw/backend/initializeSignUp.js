7d40ddf01f32c9fb05ac4d2241fb3ec48158ce0e
Gustavo Almeida - Midia3 <gustavo.almeida.midia3@infoglobo.com.br>
2016-04-11 15:38:05 -0300

    Melhoria do popin de login e register

--- www/static/js/dw/backend/initializeSignUp.js
+++ www/static/js/dw/backend/initializeSignUp.js
@@ -41,47 +41,95 @@ define(function() {
 
         refreshSalt();
 
+        function checkEmail(emailAddress) {
+          var sQtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
+          var sDtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
+          var sAtom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
+          var sQuotedPair = '\\x5c[\\x00-\\x7f]';
+          var sDomainLiteral = '\\x5b(' + sDtext + '|' + sQuotedPair + ')*\\x5d';
+          var sQuotedString = '\\x22(' + sQtext + '|' + sQuotedPair + ')*\\x22';
+          var sDomain_ref = sAtom;
+          var sSubDomain = '(' + sDomain_ref + '|' + sDomainLiteral + ')';
+          var sWord = '(' + sAtom + '|' + sQuotedString + ')';
+          var sDomain = sSubDomain + '(\\x2e' + sSubDomain + ')*';
+          var sLocalPart = sWord + '(\\x2e' + sWord + ')*';
+          var sAddrSpec = sLocalPart + '\\x40' + sDomain; // complete RFC822 email address spec
+          var sValidEmail = '^' + sAddrSpec + '$'; // as whole string
+
+          var reValidEmail = new RegExp(sValidEmail);
+
+          return reValidEmail.test(emailAddress);
+        }
+
         $('.btn-register').click(function(evt) {
+            
+            
+
             var form = $(evt.target).parents('.signup-form'),
                 pwd = $.trim($('.register-pwd', form).val()),
                 pwd2 = $.trim($('.register-pwd-2', form).val()),
-                auth_salt = $('.btn-register', form).data('salt');
+                auth_salt = $('.btn-register', form).data('salt'),
+                email = $('.register-email', form).val();
+            form.find('.btn-register').addClass('disabled');
+            form.find('.alert').remove();
+            form.find('.error').removeClass('error');
 
-           if (pwd == pwd2) {
-              if (true) {
-                 var payload = {
-                    email: $('.register-email', form).val(),
-                    pwd: CryptoJS.HmacSHA256(pwd, auth_salt).toString(),
-                    pwd2: CryptoJS.HmacSHA256(pwd2, auth_salt).toString()
-                 };
-                 $.ajax({
-                    url: '/api/users',
-                    type: 'POST',
-                    data: JSON.stringify(payload),
-                    dataType: 'json',
-                    context: this,
-                    success: function(data) {
-                        if (data.status == 'ok') {
-                            // If the registration went well, notify user we sent him an email
-                            $('.row.login-signup, .alternative-signins').addClass("hidden");
-                            $('.signup-confirm').removeClass("hidden");
+            if (email == '' || !checkEmail(email)){
+                $('.register-email', form).closest('.control-group').addClass('error');
+                dw.backend.logError('O campo email não parece válido.', '.signup-form');
+                form.find('.btn-register').removeClass('disabled');
+                return;
+            }
+            if (pwd == ''){
+                $('.register-pwd', form).closest('.control-group').addClass('error');
+                dw.backend.logError('O campo senha é obrigatório.', '.signup-form');
+                form.find('.btn-register').removeClass('disabled');
+                return;
+            }
+            if (pwd.length<8){
+                $('.register-pwd', form).closest('.control-group').addClass('error');
+                dw.backend.logError('Use senha com no mínimo 8 caracteres.', '.signup-form');
+                form.find('.btn-register').removeClass('disabled');
+                return;
+            }
+            if (pwd !== pwd2){
+                $('.register-pwd', form).closest('.control-group').addClass('error');
+                $('.register-pwd-2', form).closest('.control-group').addClass('error');
+                dw.backend.logError('O campo senha e o repetir senha não estão iguais.', '.signup-form');
+                form.find('.btn-register').removeClass('disabled');
+                return;
+            }
+             var payload = {
+                email: email,
+                pwd: CryptoJS.HmacSHA256(pwd, auth_salt).toString(),
+                pwd2: CryptoJS.HmacSHA256(pwd2, auth_salt).toString()
+             };
+             $.ajax({
+                url: '/api/users',
+                type: 'POST',
+                data: JSON.stringify(payload),
+                dataType: 'json',
+                context: this,
+                success: function(data) {
+                    if (data.status == 'ok') {
+                        // If the registration went well, notify user we sent him an email
+                        $('.row.login-signup, .alternative-signins').addClass("hidden");
+                        $('.signup-confirm').removeClass("hidden");
 
-                            $('.btn-got-it', '.signup-confirm').click(function() {
-                                $('#dwLoginForm').modal('hide');
-                                window.location.reload();
-                            });
-                        } else {
-                            dw.backend.logError(data.code, '.signup-form');
-                        }
+                        $('.btn-got-it', '.signup-confirm').click(function() {
+                            $('#dwLoginForm').modal('hide');
+                            window.location.reload();
+                        });
+                    } else {
+                        dw.backend.logError(data.code, '.signup-form');
+                        form.find('.btn-register').removeClass('disabled');
                     }
-                 });
-              } else {
-                 alert('Error: password is to unsecure. please chose a password with at least 8 characters');
-              }
-           } else {
-              alert('Error: password mismatch');
-           }
+
+                }
+             });
         });
+
+        
 
         function loginEvent(evt) {
             var loginForm = $(evt.target).parents('.login-form'),
